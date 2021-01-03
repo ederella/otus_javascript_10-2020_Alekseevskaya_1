fb365aeaa6ad98cc3d3354518b261f9493cd9b3f
Veronica Alexeevskaya <ederella@yandex.ru>
2021-01-03 17:27:24 +0300

    Tests to homework 7 added

--- src/homework7.test.js
+++ src/homework7.test.js
@@ -37,4 +37,25 @@ describe("Homework 7", () => {
       expect(el.children.length === count + 1);
     });
   });
+
+  describe("Check showButton call on events", () => {
+    it("calls shows the button on input text", () => {
+      const inp = document.querySelector(".input");
+      const event = new window.Event("input");
+      const btn = document.querySelector(".button");
+      inp.dispatchEvent(event);
+      expect(btn.hidden === false);
+    });
+  });
+
+  describe("Check addParagraphFromInput call on events", () => {
+    it("calls addParagraphFromInput on button click", () => {
+      const btn = document.querySelector(".button");
+      const event = new window.Event("click");
+      const el = document.querySelector(".div");
+      const count = el.children.length;
+      btn.dispatchEvent(event);
+      expect(el.children.length === count + 1);
+    });
+  });
 });

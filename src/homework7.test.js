b3ad54ba042c0038b9c5db8ad51df955d2a0a2fb
Veronica Alexeevskaya <ederella@yandex.ru>
2021-01-03 21:48:56 +0300

    homework7.test.js was corrected, added tests run to sanity-check.yml

--- src/homework7.test.js
+++ src/homework7.test.js
@@ -1,6 +1,7 @@
 import { pageLoad, showButton, addParagraphFromInput } from "./homework7";
 
 describe("Homework 7", () => {
+  var btn, inp, el;
   beforeEach(() => {
     document.body.innerHTML = `<div class="div">
     <p class= "text1">Сверстать страницу </p>
@@ -9,12 +10,14 @@ describe("Homework 7", () => {
    </div>
    <input class="input"></input>
    <button class="button">Кнопка</button>`;
+    btn = document.querySelector(".button");
+    inp = document.querySelector(".input");
+    el = document.querySelector(".div");
   });
 
   describe("Check pageLoad", () => {
     it("makes button hidden", () => {
       pageLoad();
-      const btn = document.querySelector(".button");
       expect(btn.hidden === true);
     });
   });
@@ -22,16 +25,13 @@ describe("Homework 7", () => {
   describe("Check showButton", () => {
     it("shows the button", () => {
       showButton();
-      const btn = document.querySelector(".button");
       expect(btn.hidden === false);
     });
   });
 
   describe("Check addParagraphFromInput", () => {
     it("creates a new paragraph", () => {
-      const inp = document.querySelector(".input");
       inp.value = "123";
-      const el = document.querySelector(".div");
       const count = el.children.length;
       addParagraphFromInput();
       expect(el.children.length === count + 1);
@@ -40,9 +40,7 @@ describe("Homework 7", () => {
 
   describe("Check showButton call on events", () => {
     it("calls shows the button on input text", () => {
-      const inp = document.querySelector(".input");
       const event = new window.Event("input");
-      const btn = document.querySelector(".button");
       inp.dispatchEvent(event);
       expect(btn.hidden === false);
     });
@@ -50,9 +48,7 @@ describe("Homework 7", () => {
 
   describe("Check addParagraphFromInput call on events", () => {
     it("calls addParagraphFromInput on button click", () => {
-      const btn = document.querySelector(".button");
       const event = new window.Event("click");
-      const el = document.querySelector(".div");
       const count = el.children.length;
       btn.dispatchEvent(event);
       expect(el.children.length === count + 1);
